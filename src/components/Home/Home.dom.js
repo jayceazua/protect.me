e5d97ac1f9c890a029a09abb9da4c1b726c6f848
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-08-02 18:38:25 -0400

    adding extra feature

--- src/components/Home/Home.dom.js
+++ src/components/Home/Home.dom.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 class Home extends Component {
     // @todo: set your inital state
     state = {
-        networks: []
+        networks: [],
+        speed: ''
     };
     wifiScanner(){
         axios.get('/api/wifi').then((response) => {
@@ -13,10 +14,18 @@ class Home extends Component {
             })
         });
     }
+
+    testSpeed() {
+        axios.get('/api/speed').then((test) => {
+            console.log(test);
+            this.setState({
+                speed: test.data
+            })
+        });
+    }
     render() {
         return (
-            <div>
-             
+<div>   
     <div className="banner">
         <div className="bg-color">
             <div className="container">
@@ -29,7 +38,7 @@ class Home extends Component {
                             <p className="big-text">Don't leave yourself vulnerable to cyber attack.</p>
                             <p className="small-text">Ensure the public network you are accessing is secure.</p>
                         </div>
-                        <a href="#feature" className="mouse-hover">
+                        <a href="/" className="mouse-hover">
                             <div className="mouse"></div>
                         </a>
                     </div>
@@ -70,7 +79,11 @@ class Home extends Component {
                             <h3 className="det-txt"> Is this network secure?</h3>
                             <h4 className="sm-txt">Here's how to tell:</h4>
                             <button className='btn btn-danger' onClick={this.wifiScanner.bind(this)}>SCAN NETWORKS</button>
-                        </hgroup>                          
+                            <button id='speed' className='btn btn-primary' onClick={this.testSpeed.bind(this)}>TEST INTERNET SPEED</button>
+                        </hgroup> 
+                        <div id='mbsSpeed1'>
+                           Your internet speed is: <span id='mbsSpeed'>{this.state.speed}</span>
+                        </div>                         
                     </div>
                 </div>
             </div>
@@ -80,29 +93,24 @@ class Home extends Component {
           <div className='col-lg-12'>
           <br/>
           <table>
+          <thead>
                 <tr>
                     <th>Network Name</th>
                     <th>Network Security</th>
                 </tr>
+          </thead>
+          <tbody>
              {
                 this.state.networks.map((network, i) => {
-                return(
-                    
-                     
-                      
-                        
-                        <tr>
+                return( 
+                        <tr key={i}>
                             <td>{network.ssid}</td>
                             <td>{network.security}</td>
-
                         </tr>
-                          
-                     
-                  
                     )
-                })
-               
+                })          
             }
+            </tbody>
              </table>
           </div>
          </div>
@@ -196,7 +204,6 @@ class Home extends Component {
                         </div>
                     </div>
                     <div className="col-xs-12">
-                       
                         <button type="submit" id="submit" name="submit" className="form contact-form-button light-form-button oswald light">SEND EMAIL</button>
                     </div>
                 </form>
@@ -204,7 +211,7 @@ class Home extends Component {
         </div>
     </section>
     
-            </div>
+</div>
         );
     }
 }
