2140ac4bcd6b824b506570bf665592772d7ad612
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-08-02 20:59:16 -0400

    done-- going to start deploying

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,28 +1,12 @@
 var buttonScanner = document.getElementById('networkTable');
-var tableHead = document.getElementById('tableHead');
-var speeder = document.getElementById('mbsSpeed1');
 var tbody = document.getElementById('tbody');
-
-
-//IIFE - this hides the element of the table head
-(function hide () {tableHead.style.display = 'none'}());
-// (function hideAgain () {speeder.style.display = 'none'}());
-
-
-
-// once clicked it will appear along with the results
-buttonScanner.onclick = setTimeout(() => {
-	if (tableHead.style.display === 'none') {
-		tableHead.style.display = 'table-header-group';
-	} else {
-		tableHead.style.display = 'none';
-	}
-}, 4000)
 
 
 
 //display loading image 
 buttonScanner.onclick = () => {
+
+
 	var img = document.createElement('IMG');
 	img.setAttribute('src', 'img/wifiLoader.gif');
 	img.setAttribute('id', 'imgLoader')
@@ -33,4 +17,6 @@ buttonScanner.onclick = () => {
 	setTimeout(() =>{
 		document.getElementById('imgLoader').style.display = 'none'
 	}, 3500);
+
+
 }
