60b334decdd27a51f036f090fdc4b0045d23af12
Veronica Elliott <Mr_Digital@Veronicas-MacBook-Air.local>
2017-08-02 20:07:12 -0400

    adding cool features

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,14 +1,36 @@
 var buttonScanner = document.getElementById('networkTable');
 var tableHead = document.getElementById('tableHead');
+var speeder = document.getElementById('mbsSpeed1');
+var tbody = document.getElementById('tbody');
 
-function hide () {tableHead.style.display = 'none'}
 
-hide();
+//IIFE - this hides the element of the table head
+(function hide () {tableHead.style.display = 'none'}());
+// (function hideAgain () {speeder.style.display = 'none'}());
 
-buttonScanner.onclick = () => {
+
+
+// once clicked it will appear along with the results
+buttonScanner.onclick = setTimeout(() => {
 	if (tableHead.style.display === 'none') {
 		tableHead.style.display = 'table-header-group';
 	} else {
 		tableHead.style.display = 'none';
 	}
+}, 4000)
+
+
+
+//display loading image 
+buttonScanner.onclick = () => {
+	var img = document.createElement('IMG');
+	img.setAttribute('src', 'img/wifiLoader.gif');
+	img.setAttribute('id', 'imgLoader')
+	img.setAttribute('width', 304);
+	img.setAttribute('height', 228);
+	tbody.append(img);
+	
+	setTimeout(() =>{
+		document.getElementById('imgLoader').style.display = 'none'
+	}, 3500);
 }
